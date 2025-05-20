0c3bb01c64161f47bc024cc056a646773d5fa467
Utpanna Pradhan <Utpanna828@gmail.com>
2025-05-20 23:29:16 +0530

    added js

--- script.js
+++ script.js
@@ -1,22 +1,76 @@
-const msgEle = document.getElementById("msg");
-//get dom msg container
+const msgEl = document.getElementById('msg');
 
 const randomNum = getRandomNumber();
-console.log("number :" ,randomNum);
 
-window.SpeechRecognitionResult = window.SpeechRecognitionResult || window.webkitSpeechRecognition;
+console.log('Number:', randomNum);
 
+window.SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
 
-let recognition = new window.SpeechRecognitionResult();
+let recognition = new window.SpeechRecognition();
+
+// Start recognition and game
 recognition.start();
 
-function onSpeak(e){
-    const msg = e.results[0][0].transript;
-    console.log(msg);
+// Capture user speak
+function onSpeak(e) {
+  const msg = e.results[0][0].transcript;
 
-}
-function getRandomNumber(){
-    return Math.floor(Math.random() * 100) + 1;
+  writeMessage(msg);
+  checkNumber(msg);
 }
 
-recognition.addEventListner('result',onSpeak);
+// Write what user speaks
+function writeMessage(msg) {
+  msgEl.innerHTML = `
+    <div>You said: </div>
+    <span class="box">${msg}</span>
+  `;
+}
+
+// Check msg against number
+function checkNumber(msg) {
+  const num = +msg;
+
+  // Check if valid number
+  if (Number.isNaN(num)) {
+    msgEl.innerHTML += '<div>That is not a valid number</div>';
+    return;
+  }
+
+  // Check in range
+  if (num > 100 || num < 1) {
+    msgEl.innerHTML += '<div>Number must be between 1 and 100</div>';
+    return;
+  }
+
+  // Check number
+  if (num === randomNum) {
+    document.body.innerHTML = `
+      <h2>Congrats! You have guessed the number! <br><br>
+      It was ${num}</h2>
+      <button class="play-again" id="play-again">Play Again</button>
+    `;
+  } else if (num > randomNum) {
+    msgEl.innerHTML += '<div>GO LOWER</div>';
+  } else {
+    msgEl.innerHTML += '<div>GO HIGHER</div>';
+  }
+}
+
+// Generate random number
+function getRandomNumber() {
+  return Math.floor(Math.random() * 100) + 1;
+}
+
+// Speak result
+recognition.addEventListener('result', onSpeak);
+
+// End SR service
+recognition.addEventListener('end', () => recognition.start());
+
+document.body.addEventListener('click', e => {
+  if (e.target.id == 'play-again') {
+    window.location.reload();
+  }
+});
